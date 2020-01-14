7d1510072323b2e39b301fbda1fa88f826746697
chwj7700 <chwj7700@naver.com>
2020-01-14 22:47:29 +0900

    dynamic

---
+++ cwj/dynamicprogramming/ContinuitySum/ContinuitySum.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int max(int a, int b);
+int main()
+{
+	int n;
+	int numArr[100000] = { 0, };
+	
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> numArr[i];
+	}
+
+	int result = -1000;
+
+	for (int i = 1; i < n; i++) {
+		numArr[i] = max(numArr[i - 1] + numArr[i], numArr[i]);
+		result = max(numArr[i], result);
+	}
+	result = max(numArr[0], result);
+	cout << result;
+}
+
+int max(int a, int b) {
+	if(a > b){
+		return a;
+	}else{
+		return b;
+	}
+}
---
+++ cwj/dynamicprogramming/dynamicprogramming/RgbWay.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int min(int a, int b);
+
+int main()
+{
+	int n;
+	int r[1000] = { 0, }, g[1000] = { 0, }, b[1000] = { 0, };
+	int r_cost[1000] = { 0, }, g_cost[1000] = { 0, }, b_cost[1000] = { 0, };
+	int check = 0;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> r_cost[i] >> g_cost[i] >> b_cost[i];
+	}
+
+	r[0] = r_cost[0];
+	g[0] = g_cost[0];
+	b[0] = b_cost[0];
+	
+	for (int i = 1; i < n; i++) {
+		r[i] = min(g[i - 1], b[i - 1]) + r_cost[i];
+		g[i] = min(r[i - 1], b[i - 1]) + g_cost[i];
+		b[i] = min(g[i - 1], r[i - 1]) + b_cost[i];
+	}
+
+	cout << min(r[n-1], min(g[n-1], b[n-1]));
+}
+
+int min(int a, int b) {
+	if (a < b) {
+		return a;
+	}
+	else {
+		return b;
+	}
+}
