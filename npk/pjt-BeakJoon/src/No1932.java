b82829280a98200ba47232abaf48b47308116b34
나평근 <skvudrms54@gmail.com>
2020-01-29 21:00:03 +0900

    정수 삼각형 성능 향상
    ver1
    메모리  36528KB
    시간 320ms
    
    ver2
    메모리 28044KB
    시간 252ms

--- npk/pjt-BeakJoon/src/No1932.java
+++ npk/pjt-BeakJoon/src/No1932.java
@@ -6,6 +6,13 @@ import java.util.ArrayList;
 public class No1932 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
+		//ver1();
+		ver2();
+	}
+	
+	
+	public static void ver1() throws NumberFormatException, IOException {
+
 		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
 		// 전체 층수를 입력 받음
 		int floor = Integer.parseInt(sc.readLine());
@@ -64,6 +71,68 @@ public class No1932 {
 		for(Integer tmpMax : intLst_tmpDpEle) {
 			
 			max = Math.max(max, tmpMax);
+		}
+		System.out.println(max);	
+	}
+	
+	
+	public static void ver2() throws NumberFormatException, IOException {
+
+		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+		// 전체 층수를 입력 받음
+		int floor = Integer.parseInt(sc.readLine());
+		int max = -1;
+		//DP의 모양은 tri와 같다
+		//ArrayList<ArrayList<Integer>> tri = new ArrayList<ArrayList<Integer>>();
+		int tri[][] = new int[floor][floor]; 
+		//ArrayList<ArrayList<Integer>> dp = new ArrayList<ArrayList<Integer>>();
+
+//		ArrayList<Integer> intLst_tmpTriEle;
+//		ArrayList<Integer> intLst_tmpDpEle;
+		String[] strArr_tmpTriEle = new String[floor];
+
+		// 입력을 받아 파싱하여 tri 리스트에 넣어줌
+		for (int i = 0;i<floor;i++) {        
+
+	
+			strArr_tmpTriEle = sc.readLine().split(" ");
+
+			for(int j = 0;j<=i;j++) {
+
+				tri[i][j]=Integer.parseInt(strArr_tmpTriEle[j]);
+			}
+			
+
+		}                
+		// 동적 계획법을 하기위한 결과값 저장소      
+		// 가장 꼭대기의 DP 값은 정상의 값과 같다	
+		
+		
+		for(int i=1;i<floor;i++) {
+
+			for(int j = 0;j<=i;j++) {
+				//DP는 이전 합 tri는 현재 계산할 값
+				if(j == 0) {
+					//dp[i][j] = dp[i-1][0] + tri[i][j]
+					tri[i][j] = tri[i-1][0] + tri[i][j];
+					
+				}else if(j == i) {
+					//dp[i][j] = dp[i-1][i-1] + tri[i][j]
+					tri[i][j] = tri[i-1][i-1] + tri[i][j];
+					
+				}else {
+					//dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-1]) + tri[i][j]
+					tri[i][j] = Math.max(tri[i-1][j], tri[i-1][j-1]) + tri[i][j];
+					
+				}        	  
+			}
+			//dp 에 값들을 쌓음
+			
+		}
+		
+
+		for (int i = 0; i < tri.length;i++) {
+			max = Math.max(max, tri[tri.length-1][i]);
 		}
 		System.out.println(max);	
 	}
